06264d831b6502c8f6ef87d54bb07943570910f3
KangOxford <kang.li2020@outlook.com>
2022-07-29 04:55:57 +0100

    V2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <charconv>
 #include "CSVRow.h"
 
 using namespace  std;
@@ -21,19 +22,34 @@ int main()
     std::unordered_map<string, std::tuple<string, string, string, string>> RunningData;
 //    std::unordered_map<string, std::tuple<unsigned long, int, int>> RunningData;
     std::ifstream file("/Users/kang/CppCourse/Trading/HFT/input.csv");
+
+//    string s1("123");
+//    string s2("122");
+//    auto judge = s1<s2;
+//    cout<< judge <<endl;
+
+    unsigned long data;
+    std::string_view sv = "124";
+    std::from_chars(sv.data(), sv.data() + sv.size(), data);
+
+
 //    for(auto& row: CSVRange(file))
 //    {
-//        std::cout << "4th Element(" << row[3] << ")\n";
-//    }
-    int i = 0;
-    for(auto& row: CSVRange(file))
-    {
-//        RunningData.insert(std::make_pair("aaa",std::make_tuple(1234567, 10, 12)));
-        auto key = "aaa";
-        if (RunningData.find(key) == RunningData.end())
-        {
-            RunningData.insert(std::make_pair((string)(row[1]),std::make_tuple(row[0], row[2], row[3], row[0])));
-        }
+//
+//        string key = std::string{row[1]};
+//        if (RunningData.find(key) == RunningData.end()) //if it is a new key
+//        {
+//            auto min_time = row[0];
+//            auto max_time = row[0];
+//            RunningData.insert(std::make_pair((string)(row[1]),std::make_tuple(min_time, row[2], row[3], max_time)));
+//        }
+//        else //if the key has already existed
+//        {
+//            auto time_value = std::get<0>(*RunningData.find(key));
+//            auto min_time = (row[0]<time_value) ? row[0] : time_value;
+//            auto max_time = row[0];
+//            RunningData.insert(std::make_pair((string)(row[1]),std::make_tuple(min_time, row[2], row[3], max_time)));
+//        }
 
 
 //        auto iter = RunningData.find("aaa");
@@ -42,8 +58,13 @@ int main()
 
 //        RunningData.insert(std::make_pair((string)(row[1]),std::make_tuple((unsigned long)(row[0]), (int)(row[2]), (int)(row[3])));
 //        std::cout << "4th Element(" << row[3] << ")\n";
-    }
-//    auto iter = RunningData.find("aaa");
+//    }
+
+
+
+
+
+    //    auto iter = RunningData.find("aaa");
 
 //    for(auto& row: CSVRange(file))
 //    {
